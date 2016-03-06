91a01e59edff3dc77e53829dffefa74711614340
Mooxmirror <lennart@mooxmirror.io>
2016-03-06 20:00:29 +0100

    Update unit tests for Executor.py

--- Runtime/test_Executor.py
+++ Runtime/test_Executor.py
@@ -1,4 +1,4 @@
-"""Run tests on Executor."""
+"""Test package for Executor.py."""
 import unittest
 import Executor
 
@@ -11,6 +11,112 @@ class TestExecutor(unittest.TestCase):
         syntax_tree = Executor.syntax_tree()
         self.assertTrue(syntax_tree is not None)
         self.assertEqual(syntax_tree.type, Executor.TYPES[Executor._SEQUENCE])
+
+    def test_namespace(self):
+        """Test the Namespace class."""
+        ns = Executor.Namespace(None)
+        self.assertEqual(ns.parent, None)
+        self.assertEqual(ns.find_identifier("identifier"), None)
+        self.assertEqual(ns.find_operator("operator"), None)
+        # store example operator
+        ns.put_identifier("a", 0)
+        ns.put_operator("a", 1)
+        self.assertEqual(ns.find_identifier("a"), 0)
+        self.assertEqual(ns.find_operator("a"), 1)
+        # test upper namspace
+        sub = Executor.Namespace(ns)
+        self.assertEqual(sub.find_identifier("a"), 0)
+        self.assertEqual(sub.find_operator("a"), 1)
+        # check independence
+        sub.put_identifier("missing", -1)
+        sub.put_operator("missing", -2)
+        self.assertEqual(ns.find_identifier("missing"), None)
+        self.assertEqual(ns.find_operator("missing"), None)
+
+    def test_default_namespace(self):
+        """Test the default_namespace method."""
+        ns = Executor.default_namespace()
+        self.assertEqual(ns.parent, None)
+        self.assertEqual(ns.find_identifier(""), None)
+        self.assertEqual(ns.find_operator(""), None)
+
+    def test_run_in_substitution(self):
+        """Test the run_in_substitution method."""
+        access_node_type = {
+            "name": "Test",
+            "execution": lambda n, c: c["local"].put_identifier("x", 0),
+        }
+        context = Executor.default_context()
+        access_node = Executor.Node(access_node_type, None)
+        Executor.run_in_substitution(access_node, context)
+        self.assertEqual(context["local"].find_identifier("x"), None)
+
+        value_node_type = {
+            "name": "Test",
+            "execution": lambda n, c: Executor.store_none(),
+        }
+        context = Executor.default_context()
+        value_node = Executor.Node(value_node_type, None)
+        value_result = Executor.run_in_substitution(value_node, context)
+        self.assertEqual(value_result, Executor.store_none())
+
+    def test_default_context(self):
+        """Test the default_context method."""
+        pass
+
+    def test_store_none(self):
+        """Test the store_none method."""
+        a = Executor.store_none()
+        b = Executor.store_none()
+        self.assertEqual(a["type"], Executor.DATA_NONE)
+        self.assertEqual(a["value"], None)
+        self.assertEqual(a, b)
+
+    def test_store_value(self):
+        """Test the store_value method."""
+
+    def test_sequence_node(self):
+        """Test the sequence node."""
+        sequence = Executor.Node(Executor.TYPES[Executor._SEQUENCE], None)
+        context = Executor.default_context()
+        predicted_result = Executor.store_none()
+        self.assertEqual(sequence.eval(context), predicted_result)
+
+    def test_branch_node(self):
+        """Test the branch node."""
+        pass
+
+    def test_loop_node(self):
+        """Test the loop node."""
+        pass
+
+    def test_return_node(self):
+        """Test the return node."""
+        pass
+
+    def test_break_node(self):
+        """Test the break node."""
+        pass
+
+    def test_continue_node(self):
+        """Test the continue node."""
+        pass
+
+    def test_function_node(self):
+        """Test the function node."""
+        pass
+
+    def test_operator_node(self):
+        """Test the operator node."""
+        pass
+
+    def test_identifier_node(self):
+        """Test the identifier node."""
+        pass
+
+    def test_literal_node(self):
+        """Test the literal node."""
+        pass
 
 if __name__ == "__main__":
     unittest.main()
