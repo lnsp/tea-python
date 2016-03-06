e33534e531df1dbbc3d47eafb5c181f81c1cbffc
Mooxmirror <lennart@mooxmirror.io>
2016-03-06 20:00:03 +0100

    Add loop and namespace substitution

--- Runtime/Executor.py
+++ Runtime/Executor.py
@@ -2,6 +2,8 @@
 import Utils
 
 BEHAVIOUR_DEFAULT = "default"  # do nothing special
+BEHAVIOUR_BREAK = "break"
+BEHAVIOUR_CONTINUE = "continue"
 BEHAVIOUR_RETURN = "return"  # return, but reset to nothing
 BEHAVIOUR_EXIT = "exit"  # exit from program
 
@@ -11,11 +13,16 @@ DATA_BOOLEAN = "Boolean"  # true / false
 DATA_STRING = "String"  # Any string value
 DATA_FUNCTION = "Function"
 DATA_NONE = "None"  # None / null value
-# TODO: Add lists, maps and sets
+DATA_LIST = "List"
+DATA_MAPS = "Map"
+DATA_SET = "Set"
 
 _SEQUENCE = "sequence"
 _BRANCH = "branch"
+_LOOP = "loop"
 _RETURN = "return"
+_BREAK = "break"
+_CONTINUE = "continue"
 _FUNCTION = "function"
 _OPERATOR = "operator"
 _IDENTIFIER = "identifier"
@@ -92,7 +99,7 @@ class Namespace:
         # search in local namespace
         for key in self.identifiers:
             if key == ident:
-                return self.items[key]
+                return self.identifiers[key]
         # find in parent namespaces
         if self.parent is not None:
             return self.parent.find_identifier(ident)
@@ -111,15 +118,24 @@ class Namespace:
         # return if nothing available
         return None
 
-    def put_item(self, item):
+    def put_identifier(self, name, item):
         """Store identifier in namespace."""
         # add item to namespace
-        self.identifiers[item["name"]] = item
+        self.identifiers[name] = item
 
-    def put_operator(self, operator):
+    def put_operator(self, symbol, operator):
         """Store operator in namespace."""
         # add operator to namespace
-        self.operator[operator["name"]] = operator
+        self.operators[symbol] = operator
+
+
+def run_in_substitution(node, context):
+    """Run the node in a subtituted namespace."""
+    sub = Namespace(context["local"])
+    context["local"] = sub
+    result = node.eval(context)
+    context["local"] = sub.parent
+    return result
 
 
 def eval_sequence(node, context):
@@ -139,8 +155,43 @@ def eval_branch(node, context):
     """Evaluate a n-component branch (if, else-if ..., else)."""
     for branch in node.children[:-1]:  # all if / else if branches
         if branch.children[0].eval(context)["value"]:  # condition ->
-            return branch.children[1].eval(context)  # execution
-    return branch.children[-1].eval(context)  # else branch
+            return run_in_substitution(branch.children[1])
+    return run_in_substitution(branch.children[-1])
+
+
+def eval_loop(node, context):
+    """Evaluate either a 2-component or 4-component loop."""
+    n = len(node.children)
+    # substitute for loop context
+    loop_ns = Namespace(context["local"])
+    context["local"] = loop_ns
+    if n == 2:  # while-loop
+        behaviour = context["behaviour"]
+        while node.children[0].eval(context)["value"]:
+            context["behaviour"] = BEHAVIOUR_DEFAULT
+            result = node.children[1].eval(context)
+            behaviour = context["behaviour"]
+            if behaviour == BEHAVIOUR_RETURN:
+                return result
+            if behaviour == BEHAVIOUR_BREAK:
+                break
+        return store_none()
+    elif n == 4:
+        # execute start statement
+        node.children[0].eval(context)
+        # execute loop
+        while node.children[1].eval(context)["value"]:
+            context["behaviour"] = BEHAVIOUR_DEFAULT
+            # execute statements
+            result = node.children[2].eval(context)
+            behaviour = context["behaviour"]
+            if behaviour == BEHAVIOUR_RETURN:
+                return result
+            if behaviour == BEHAVIOUR_BREAK:
+                break
+            # execute iterator
+            node.children[3].eval(context)
+        return store_none()
 
 
 def eval_operator(node, context):
@@ -151,7 +202,7 @@ def eval_operator(node, context):
     operator = context.find_operator(node.data["symbol"])
     if operator is not None:
         values = [child.eval(context) for child in node.children]
-        return operator["execution"](values)
+        return operator["execution"](context, values)
     raise "Operator not found"
 
 
@@ -163,7 +214,14 @@ def eval_function(node, context):
     function = context.find_identifier(node.data["name"])
     if function is not None:
         values = [child.eval(context) for child in node.children]
-        return function["execution"](values)
+        # substitute namespace
+        parent_ns = context["local"]
+        child_ns = Namespace(context["global"])
+        context["local"] = child_ns
+        # execute function
+        result = function["execution"](context, values)
+        context["local"] = parent_ns
+        return result
     raise "Function not found"
 
 
@@ -201,6 +259,17 @@ def eval_return(node, context):
     return value
 
 
+def eval_break(node, context):
+    """Evaluate a break statement."""
+    context["behaviour"] = BEHAVIOUR_BREAK
+    return store_none()
+
+
+def eval_continue(node, context):
+    """Evaluate a continue statement."""
+    context["continue"] = BEHAVIOUR_CONTINUE
+    return store_none()
+
 TYPES = {
     _SEQUENCE: {
         "name": _SEQUENCE,
@@ -209,6 +278,10 @@ TYPES = {
     _BRANCH: {
         "name": _BRANCH,
         "execution": eval_branch,
+    },
+    _LOOP: {
+        "name": _LOOP,
+        "execution": eval_loop,
     },
     _OPERATOR: {
         "name": _OPERATOR,
@@ -224,7 +297,15 @@ TYPES = {
     },
     _RETURN: {
         "name": _RETURN,
-        "execution": None,
+        "execution": eval_return,
+    },
+    _BREAK: {
+        "name": _BREAK,
+        "execution": eval_break,
+    },
+    _CONTINUE: {
+        "name": _CONTINUE,
+        "execution": eval_continue,
     },
     _FUNCTION: {
         "name": _FUNCTION,
@@ -242,7 +323,7 @@ def default_namespace():
 def default_context():
     """Initialize a default context."""
     context = Utils.tree()
-    context["namespace"] = default_namespace()
+    context["local"] = context["global"] = default_namespace()
     return context
 
 
