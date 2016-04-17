1aed7278fbb7da8c3074fe44e6b4399bc1c259bf
Lennart Espe <lennart@mooxmirror.io>
2016-04-17 15:11:24 +0200

    Rework type system

--- Runtime/Executor.py
+++ Runtime/Executor.py
@@ -6,16 +6,6 @@ BEHAVIOUR_BREAK = "break"
 BEHAVIOUR_CONTINUE = "continue"
 BEHAVIOUR_RETURN = "return"  # return, but reset to nothing
 
-DATA_INTEGER = "Integer"  # Any integer number
-DATA_FLOAT = "Float"  # Any floating point number
-DATA_BOOLEAN = "Boolean"  # true / false
-DATA_STRING = "String"  # Any string value
-DATA_FUNCTION = "Function"
-DATA_NONE = "None"  # None / null value
-DATA_LIST = "List"
-DATA_MAP = "Map"
-DATA_SET = "Set"
-
 _SEQUENCE = "sequence"
 _CONDITIONAL = "conditional"
 _BRANCH = "branch"
@@ -28,17 +18,96 @@ _OPERATOR = "operator"
 _IDENTIFIER = "identifier"
 _LITERAL = "literal"
 
+class Value:
+    """A variable storing a value with a specific type."""
+    def __init__(self, datatype, data):
+        self.datatype = datatype
+        self.data = data
+    def __eq__(self, other):
+        return (isinstance(other, self.__class__)
+            and self.datatype == other.datatype
+            and self.data == other.data)
+    def __ne__(self, other):
+        return not self.__eq__(other) 
 
-def store_value(t, v):
-    """Create a storage with a stored type and value."""
-    return {"type": t, "value": v}
+class Signature:
+    """A signature matching a function call."""
+    def __init__(self, expected, function):
+        self.expected = expected
+        self.function = function
+        
+    def match(self, called):
+        """Checks if the arguments match the function signature.
+        If yes, it returns (Arguments, Function)
+        If no, it raises an error.
+        """
+        expected_n, called_n = len(self.expected), len(called)
+        if expected_n > called_n:
+            raise Exception("Too less arguments")
+        elif expected_n < called_n:
+            raise Exception("Too many arguments")
+        args = [self.expected[n].cast(called[n]) for n in range(expected_n)]
+        return args, self.function
 
+class Type:
+    """A type representing a basic type."""
+    
+    def __init__(self, name, cast=None):
+        self.name = name
+        self.cast = cast
 
-def store_none():
-    """Create a storage with no value."""
-    return store_value(DATA_NONE, None)
+datatypes = {
+    "int": Type("int"),
+    "float": Type("float"),
+    "bool": Type("bool"),
+    "string": Type("string"),
+    "func": Type("function"),
+    "null": Type("null"),
+    "list": Type("list"),
+    "map": Type("map"),
+    "set": Type("set"),
+}
 
+def store_null():
+    return Value(datatypes["null"], None)
 
+class Function:
+    """A function with a collection of signatures."""
+    def __init__(self, signatures, name=None):
+        self.signatures = signatures
+        self.name = name
+        
+    def eval(self, args, context):
+        for sgn in self.signatures:
+            try:
+                values, fnc = match(args)
+                return fnc(context, values)
+            except: pass
+        raise Exception("No matching signature found")
+
+class Operator:
+    """A operator with a collection of signatures and functions."""
+    
+    def __init__(self, function, symbol):
+        self.function = function
+        self.symbol = symbol
+        
+    def eval(self, args, context):
+        return self.function.eval(args, context)
+
+class Identifier:
+    """A identifier with a name and a value."""
+    
+    def __init__(self, value, name=None):
+        self.datatype = value.datatype
+        self.data = value.data
+        self.name = name
+    def __eq__(self, other):
+        return ((isinstance(other, Identifier) or isinstance(other, Value))
+            and self.datatype == other.datatype
+            and self.data == other.data)
+    def __ne__(self, other):
+        return not self.__eq__(other) 
 
 class Node:
     """A node in the abstract syntax tree."""
@@ -56,7 +125,7 @@ class Node:
     def add(self, node):
         """Add a child to the node."""
         self.children.append(node)
-        
+
 
 class Namespace:
     """A variable and operator namespace."""
@@ -69,40 +138,54 @@ class Namespace:
         self.identifiers = {}
         # operator map
         self.operators = {}
+        # type map
+        self.types = {}
+        # search spaces
+        self.spaces = {
+            "type": self.types,
+            "operator": self.operators,
+            "identifier": self.identifiers,
+        }
+        
+    def find(self, space, key):
+        """Search for an element in this and higher order namespaces."""
+        # search in local namespace
+        try:
+            return self.spaces[space][key]
+        except: pass
+        # find in parent namespaces
+        if self.parent is not None:
+            return self.parent.find(space, key)
+        # return if nothing available
+        return None
 
     def find_identifier(self, ident):
         """Search for an identifier in this and higher order namespaces."""
-        # search in local namespace
-        for key in self.identifiers:
-            if key == ident:
-                return self.identifiers[key]
-        # find in parent namespaces
-        if self.parent is not None:
-            return self.parent.find_identifier(ident)
-        # return if nothing available
-        return None
+        return self.find("identifier", ident)
 
     def find_operator(self, op):
         """Search for an operator in this and higher order namespaces."""
-        # search in local namespace
-        for key in self.operators:
-            if key == op:
-                return self.operators[key]
-        # find in parent namespaces
-        if self.parent is not None:
-            return self.parent.find_operator(op)
-        # return if nothing available
-        return None
+        return self.find("operator", op)
+    
+    def find_type(self, t):
+        """Search for a type in this and higher order namespaces."""
+        return self.find("type", t)
+        
+    def store(self, space, name, item):
+        """Stores a item in the specified search space."""
+        self.spaces[space][name] = item;
 
-    def put_identifier(self, name, item):
+    def put_identifier(self, item):
         """Store identifier in namespace."""
-        # add item to namespace
-        self.identifiers[name] = item
+        self.store("identifier", item.name, item)
 
-    def put_operator(self, symbol, operator):
+    def put_operator(self, operator):
         """Store operator in namespace."""
-        # add operator to namespace
-        self.operators[symbol] = operator
+        self.store("operator", operator.symbol, operator)
+        
+    def put_type(self, t):
+        """Store type in namespace."""
+        self.store("type", t.name, t)
 
 class Context:
     """A context for temporary storage."""
@@ -131,7 +214,7 @@ def run_in_substitution(node, context):
 def eval_sequence(node, context):
     """Evaluate a sequence of statements."""
     context.behaviour = BEHAVIOUR_DEFAULT
-    last_value = store_none()
+    last_value = store_null()
 
     for item in node.children:
         last_value = item.eval(context)
@@ -152,10 +235,10 @@ def eval_branch(node, context):
 def eval_conditional(node, context):
     """Evaluate a conditional (if [0] then [1])."""
     correct = node.children[0].eval(context)
-    if correct["type"] is not DATA_BOOLEAN:
+    if correct.datatype is not datatypes["bool"]:
         raise Exception("Bad conditional")
     else:
-        if correct["value"]:
+        if correct.data:
             return run_in_substitution(node.children[1], context)
         else:
             return False
@@ -168,43 +251,41 @@ def eval_loop(node, context):
         if bhv == BEHAVIOUR_RETURN: return cond
         else:
             context.behaviour = BEHAVIOUR_DEFAULT
-            if bhv == BEHAVIOUR_BREAK: return store_none()
+            if bhv == BEHAVIOUR_BREAK: return store_null()
         cond = eval_conditional(node, context)
-    return store_none()
+    return store_null()
 
 def eval_operator(node, context):
     """Evaluate an operator and return the result.
 
-    node.data['symbol'] is required for execution.
+    node.data = symbol is required for execution.
     """
     operator = context.find_operator(node.data)
     if operator is not None:
         args = [child.eval(context) for child in node.children]
-        return operator["execution"](context, args)
+        return operator.eval(args, context)
     raise Exception("Operator not found")
 
 
 def eval_function(node, context):
     """Evaluate a function and return the result.
 
-    node.data['name'] is required for execution.
+    node.data is required for execution.
     """
     function = context.find_identifier(node.data)
     if function is not None:
         args = [child.eval(context) for child in node.children]
-        result = function["execution"](context, args)
-        return result
+        return function.eval(args, context)
     raise Exception("Function not found")
 
 
 def eval_identifier(node, context):
     """Evaluate an identifier and return the result.
 
-    node.data['name'] is required for execution.
+    node.data = name is required for execution.
     """
     identifier = context.find_identifier(node.data)
     if identifier is not None:
-        identifier["name"] = node.data
         return identifier
     raise Exception("Identifier not found")
 
@@ -230,13 +311,13 @@ def eval_return(node, context):
 def eval_break(node, context):
     """Evaluate a break statement."""
     context.behaviour = BEHAVIOUR_BREAK
-    return store_none()
+    return store_null()
 
 
 def eval_continue(node, context):
     """Evaluate a continue statement."""
     context.behaviour = BEHAVIOUR_CONTINUE
-    return store_none()
+    return store_null()
 
 TYPES = {
     _SEQUENCE: {
--- Runtime/test_Executor.py
+++ Runtime/test_Executor.py
@@ -1,17 +1,21 @@
 """Test package for Executor.py."""
 import unittest
 from Runtime import Executor
+from Runtime.Executor import Value, Node, store_null, datatypes, Identifier, Operator, Function
+
+
 
 def new_node(typename, data=None):
-    return Executor.Node(Executor.TYPES[typename], data)
+    return Node(Executor.TYPES[typename], data)
     
 def new_literal(value):
     return new_node(Executor._LITERAL, value)
     
-none_literal = new_literal(Executor.store_none())
-true_literal = new_literal(Executor.store_value(Executor.DATA_BOOLEAN, True))
-false_literal = new_literal(Executor.store_value(Executor.DATA_BOOLEAN, False))
-string_literal = new_literal(Executor.store_value(Executor.DATA_STRING, "works"))
+null_literal = new_literal(store_null())
+int_literal = new_literal(Value(datatypes["int"], 0))
+true_literal = new_literal(Value(datatypes["bool"], True))
+false_literal = new_literal(Value(datatypes["bool"], False))
+string_literal = new_literal(Value(datatypes["string"], "works"))
         
 
 class TestExecutor(unittest.TestCase):
@@ -30,19 +34,19 @@ class TestExecutor(unittest.TestCase):
         self.assertEqual(ns.find_identifier("identifier"), None)
         self.assertEqual(ns.find_operator("operator"), None)
         # store example operator
-        ns.put_identifier("a", 0)
-        ns.put_operator("a", 1)
-        self.assertEqual(ns.find_identifier("a"), 0)
-        self.assertEqual(ns.find_operator("a"), 1)
+        ns.put_identifier(Identifier(string_literal.data, "str"))
+        ns.put_operator(Operator(None, "+"))
+        self.assertEqual(ns.find_identifier("str"), string_literal.data)
+        self.assertEqual(ns.find_operator("+").symbol, "+")
         # test upper namspace
         sub = Executor.Namespace(ns)
-        self.assertEqual(sub.find_identifier("a"), 0)
-        self.assertEqual(sub.find_operator("a"), 1)
+        self.assertEqual(ns.find_identifier("str"), string_literal.data)
+        self.assertEqual(ns.find_operator("+").symbol, "+")
         # check independence
-        sub.put_identifier("missing", -1)
-        sub.put_operator("missing", -2)
+        sub.put_identifier(Identifier(true_literal.data, "missing"))
+        sub.put_operator(Operator(None, "-"))
         self.assertEqual(ns.find_identifier("missing"), None)
-        self.assertEqual(ns.find_operator("missing"), None)
+        self.assertEqual(ns.find_operator("-"), None)
 
     def test_default_namespace(self):
         """Test the default_namespace method."""
@@ -55,7 +59,7 @@ class TestExecutor(unittest.TestCase):
         """Test the run_in_substitution method."""
         access_node_type = {
             "name": "Test",
-            "execution": lambda n, c: c.ns.put_identifier("x", 0),
+            "execution": lambda n, c: c.ns.put_identifier(Identifier(int_literal.data, "x")),
         }
         context = Executor.default_context()
         access_node = Executor.Node(access_node_type, None)
@@ -64,31 +68,25 @@ class TestExecutor(unittest.TestCase):
 
         value_node_type = {
             "name": "Test",
-            "execution": lambda n, c: Executor.store_none(),
+            "execution": lambda n, c: Executor.store_null(),
         }
         context = Executor.default_context()
         value_node = Executor.Node(value_node_type, None)
         value_result = Executor.run_in_substitution(value_node, context)
-        self.assertEqual(value_result, Executor.store_none())
+        self.assertEqual(value_result, Executor.store_null())
 
     def test_default_context(self):
         """Test the default_context method."""
         context = Executor.default_context()
         self.assertEqual(context.globalns, context.ns)
 
-    def test_store_none(self):
-        """Test the store_none method."""
-        a = Executor.store_none()
-        b = Executor.store_none()
-        self.assertEqual(a["type"], Executor.DATA_NONE)
-        self.assertEqual(a["value"], None)
+    def test_store_null(self):
+        """Test the store_null method."""
+        a = Executor.store_null()
+        b = Executor.store_null()
+        self.assertEqual(a.datatype, Executor.datatypes["null"])
+        self.assertEqual(a.data, None)
         self.assertEqual(a, b)
-
-    def test_store_value(self):
-        """Test the store_value method."""
-        i = Executor.store_value(Executor.DATA_NONE, None)
-        self.assertEqual(i["type"], Executor.DATA_NONE)
-        self.assertEqual(i["value"], None)
         
     def test_sequence_node(self):
         """Test the sequence node."""
@@ -97,18 +95,18 @@ class TestExecutor(unittest.TestCase):
         return_node.children = [true_literal]
         # empty sequence
         empty_seq = new_node(Executor._SEQUENCE)
-        self.assertEqual(empty_seq.eval(context), none_literal.data)
+        self.assertEqual(empty_seq.eval(context), null_literal.data)
         
         # non-empty sequence
         non_seq = new_node(Executor._SEQUENCE)
-        non_seq.children = [none_literal, true_literal]
+        non_seq.children = [null_literal, true_literal]
         self.assertEqual(non_seq.eval(context), true_literal.data)
-        non_seq.children = [true_literal, none_literal]
-        self.assertEqual(non_seq.eval(context), none_literal.data)
+        non_seq.children = [true_literal, null_literal]
+        self.assertEqual(non_seq.eval(context), null_literal.data)
         
         # sequence with return
         ret_seq = new_node(Executor._SEQUENCE)
-        ret_seq.children = [return_node, none_literal]
+        ret_seq.children = [return_node, null_literal]
         self.assertEqual(ret_seq.eval(context), true_literal.data)
         
 
@@ -118,15 +116,15 @@ class TestExecutor(unittest.TestCase):
         
         # Test bad conditional error
         bad_conditional = new_node(Executor._CONDITIONAL)
-        bad_conditional.add(none_literal) # if None:
-        bad_conditional.add(none_literal) # then None
+        bad_conditional.add(null_literal) # if None:
+        bad_conditional.add(null_literal) # then None
         self.assertRaises(Exception, bad_conditional.eval, context)
         
         # Test correct result
         good_conditional = new_node(Executor._CONDITIONAL)
         good_conditional.add(true_literal)
-        good_conditional.add(none_literal)
-        self.assertEqual(good_conditional.eval(context), none_literal.data)
+        good_conditional.add(null_literal)
+        self.assertEqual(good_conditional.eval(context), null_literal.data)
 
     def test_branch_node(self):
         """Test the branch node."""
@@ -135,10 +133,10 @@ class TestExecutor(unittest.TestCase):
         true_cond = new_node(Executor._CONDITIONAL)
         true_cond.children = [true_literal, string_literal]
         false_cond = new_node(Executor._CONDITIONAL)
-        false_cond.children = [false_literal, none_literal]
+        false_cond.children = [false_literal, null_literal]
         # Test if branch
         if_branch = new_node(Executor._BRANCH)
-        if_branch.children = [true_cond, none_literal]
+        if_branch.children = [true_cond, null_literal]
         self.assertEqual(if_branch.eval(context), string_literal.data)
         # Test if-else branch
         ifelse_branch = new_node(Executor._BRANCH)
@@ -146,7 +144,7 @@ class TestExecutor(unittest.TestCase):
         self.assertEqual(ifelse_branch.eval(context), string_literal.data)
         # Test if-elif-else branch
         ifelifelse_branch = new_node(Executor._BRANCH)
-        ifelifelse_branch.children = [false_cond, true_cond, none_literal]
+        ifelifelse_branch.children = [false_cond, true_cond, null_literal]
         self.assertEqual(ifelifelse_branch.eval(context), string_literal.data)
 
     def test_loop_node(self):
@@ -158,13 +156,13 @@ class TestExecutor(unittest.TestCase):
         
         # check for exception
         bad_loop = new_node(Executor._LOOP)
-        bad_loop.children = [none_literal, true_literal]
+        bad_loop.children = [null_literal, true_literal]
         self.assertRaises(Exception, bad_loop.eval, context)
         
         # check for break 
         break_loop = new_node(Executor._LOOP)
         break_loop.children = [true_literal, break_node]
-        self.assertEqual(break_loop.eval(context), none_literal.data)
+        self.assertEqual(break_loop.eval(context), null_literal.data)
         self.assertEqual(context.behaviour, Executor.BEHAVIOUR_DEFAULT)
         
         # check for return
@@ -178,7 +176,7 @@ class TestExecutor(unittest.TestCase):
         # test empty return node
         context = Executor.default_context()
         empty_return = new_node(Executor._RETURN)
-        self.assertEqual(empty_return.eval(context), none_literal.data)
+        self.assertEqual(empty_return.eval(context), null_literal.data)
         self.assertEqual(context.behaviour, Executor.BEHAVIOUR_RETURN)
         
         # test return with value
@@ -192,14 +190,14 @@ class TestExecutor(unittest.TestCase):
         """Test the break node."""
         context = Executor.default_context()
         break_node = new_node(Executor._BREAK)
-        self.assertEqual(break_node.eval(context), none_literal.data)
+        self.assertEqual(break_node.eval(context), null_literal.data)
         self.assertEqual(context.behaviour, Executor.BEHAVIOUR_BREAK)
 
     def test_continue_node(self):
         """Test the continue node."""
         context = Executor.default_context()
         continue_node = new_node(Executor._CONTINUE)
-        self.assertEqual(continue_node.eval(context), none_literal.data)
+        self.assertEqual(continue_node.eval(context), null_literal.data)
         self.assertEqual(context.behaviour, Executor.BEHAVIOUR_CONTINUE)
 
     def test_function_node(self):
@@ -207,20 +205,22 @@ class TestExecutor(unittest.TestCase):
         pass
 
     def test_operator_node(self):
-        """Test the operator node."""
+        """Test the operator node.
+        A operator has one or more call signatures, that match a function call.
+        """
         pass
 
     def test_identifier_node(self):
         """Test the identifier node."""
         context = Executor.default_context()
         # Search in local ns
-        context.ns.put_identifier("i", string_literal.data)
+        context.ns.put_identifier(Identifier(string_literal.data, "i"))
         ident_node = new_node(Executor._IDENTIFIER, "i")
-        self.assertEqual(ident_node.eval(context)["value"], string_literal.data["value"])
+        self.assertEqual(ident_node.eval(context).data, string_literal.data.data)
         
         # Search in parent ns
         context.ns = Executor.Namespace(context.ns)
-        self.assertEqual(ident_node.eval(context)["value"], string_literal.data["value"])
+        self.assertEqual(ident_node.eval(context).data, string_literal.data.data)
         
         # Identifier does not exist
         bad_node = new_node(Executor._IDENTIFIER, "nope")
@@ -229,7 +229,7 @@ class TestExecutor(unittest.TestCase):
     def test_literal_node(self):
         """Test the literal node."""
         context = Executor.default_context()
-        self.assertEqual(none_literal.eval(context), none_literal.data)
+        self.assertEqual(null_literal.eval(context), null_literal.data)
         self.assertEqual(string_literal.eval(context), string_literal.data)
         self.assertEqual(true_literal.eval(context), true_literal.data)
         self.assertEqual(false_literal.eval(context), false_literal.data)
