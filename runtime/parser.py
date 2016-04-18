afd1f98ce86b2cead544213700ab31ba96c16b6c
Lennart Espe <lennart@mooxmirror.io>
2016-04-19 00:34:18 +0200

    Update parser to match current project structure

--- runtime/parser.py
+++ runtime/parser.py
@@ -1,5 +1,5 @@
 """Parse an tokenized expression into an AST."""
-from runtime import ast, std
+from runtime import ast
 
 
 def generate(tokens):
@@ -11,10 +11,4 @@ def generate(tokens):
 def demo_syntax_tree():
     """Initialize a demo syntax tree."""
     tree = ast.syntax_tree()
-
-    literal_node = ast.Literal(std.Value(std.Boolean, True))
-    cast_node = ast.Cast(std.Integer.name)
-    cast_node.add(literal_node)
-    tree.add(cast_node)
-
     return tree
