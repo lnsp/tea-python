b4bff8e7bb87ec2c16830d91eddf6a580bcc716d
Lennart Espe <lennart@mooxmirror.io>
2016-04-19 01:31:27 +0200

    Update tests for runtime.lib

--- runtime/test_lib.py
+++ runtime/test_lib.py
@@ -1,123 +1,114 @@
+"""The unit test for the runtime.lib module."""
 import unittest
 
 from runtime import env, lib
 
-int_value = env.Value(lib.Integer, 1)
-int2_value = env.Value(lib.Integer, 2)
-float_value = env.Value(lib.Float, 1.0)
-float2_value = env.Value(lib.Float, 2.0)
-string_value = env.Value(lib.String, "Hello", "identifier")
-string1_value = env.Value(lib.String, "Hello1")
-list_value = env.Value(lib.List, ["H", "e", "l", "l", "o"])
-set_value = env.Value(lib.Set, set(list_value.data))
-bool_value = env.Value(lib.Boolean, True, "b")
-true_str_value = env.Value(lib.String, "true")
-missing_value = env.Value(lib.Integer, 0, "missing")
-null_value = env.Value(env.Null)
-example_operator = env.Operator(None, "+")
-missing_operator = env.Operator(None, "?")
-empty_function = env.Value(lib.Func, None)
-object_value = env.Value(lib.Object, None)
+INT_VALUE = env.Value(lib.INTEGER, 1)
+INT2_VALUE = env.Value(lib.INTEGER, 2)
+FLOAT_VALUE = env.Value(lib.FLOAT, 1.0)
+FLOAT2_VALUE = env.Value(lib.FLOAT, 2.0)
+STRING_VALUE = env.Value(lib.STRING, "Hello", "identifier")
+STRING1_VALUE = env.Value(lib.STRING, "Hello1")
+LIST_VALUE = env.Value(lib.LIST, ["H", "e", "l", "l", "o"])
+SET_VALUE = env.Value(lib.SET, set(LIST_VALUE.data))
+BOOL_VALUE = env.Value(lib.BOOLEAN, True, "b")
+TRUE_STRING_VALUE = env.Value(lib.STRING, "true")
+MISSING_INT_VALUE = env.Value(lib.INTEGER, 0, "missing")
+NULL_VALUE = env.Value(env.NULL)
+USELESS_OPERATOR = env.Operator(None, "+")
+ANOTHER_USELESS_OPERATOR = env.Operator(None, "?")
+USELESS_FUNCTION = env.Value(lib.FUNCTION, None)
+OBJECT_VALUE = env.Value(lib.OBJECT, None)
 
 
 class TestLib(unittest.TestCase):
+    """Test cases for runtime library."""
 
     def test_integer(self):
         """Test the Integer type."""
-        self.assertEqual(lib.Integer.cast(int_value), int_value)
-        self.assertEqual(lib.Integer.cast(float_value), int_value)
-        self.assertRaises(env.CastError, lib.Integer.cast, string_value)
+        self.assertEqual(lib.INTEGER.cast(INT_VALUE), INT_VALUE)
+        self.assertEqual(lib.INTEGER.cast(FLOAT_VALUE), INT_VALUE)
+        self.assertRaises(env.CastError, lib.INTEGER.cast, STRING_VALUE)
 
     def test_float(self):
-        """Test the Float type."""
-        self.assertEqual(lib.Float.cast(float_value), float_value)
-        self.assertEqual(lib.Float.cast(int_value), float_value)
-        self.assertRaises(env.CastError, lib.Float.cast, string_value)
+        """Test the FLOAT type."""
+        self.assertEqual(lib.FLOAT.cast(FLOAT_VALUE), FLOAT_VALUE)
+        self.assertEqual(lib.FLOAT.cast(INT_VALUE), FLOAT_VALUE)
+        self.assertRaises(env.CastError, lib.FLOAT.cast, STRING_VALUE)
 
     def test_string(self):
-        """Test the String type."""
-        self.assertEqual(lib.String.cast(int_value),
-                         env.Value(lib.String, str(int_value.data)))
-        self.assertEqual(lib.String.cast(bool_value),
-                         env.Value(lib.String, "true"))
-        self.assertEqual(lib.String.cast(null_value),
-                         env.Value(lib.String, "null"))
-        self.assertEqual(lib.String.cast(string_value), string_value)
-        self.assertRaises(env.CastError, lib.String.cast,
+        """Test the STRING type."""
+        self.assertEqual(lib.STRING.cast(INT_VALUE),
+                         env.Value(lib.STRING, str(INT_VALUE.data)))
+        self.assertEqual(lib.STRING.cast(BOOL_VALUE),
+                         env.Value(lib.STRING, "true"))
+        self.assertEqual(lib.STRING.cast(NULL_VALUE),
+                         env.Value(lib.STRING, "null"))
+        self.assertEqual(lib.STRING.cast(STRING_VALUE), STRING_VALUE)
+        self.assertRaises(env.CastError, lib.STRING.cast,
                           env.Value(env.Function))
 
     def test_boolean(self):
-        """Test the Boolean type."""
-        self.assertEqual(lib.Boolean.cast(bool_value), bool_value)
-        self.assertEqual(lib.Boolean.cast(int_value), bool_value)
-        self.assertRaises(env.CastError, lib.Boolean.cast, float_value)
+        """Test the BOOLEAN type."""
+        self.assertEqual(lib.BOOLEAN.cast(BOOL_VALUE), BOOL_VALUE)
+        self.assertEqual(lib.BOOLEAN.cast(INT_VALUE), BOOL_VALUE)
+        self.assertRaises(env.CastError, lib.BOOLEAN.cast, FLOAT_VALUE)
 
     def test_func(self):
-        """Test the Func type."""
-        self.assertEqual(lib.Func.cast(empty_function), empty_function)
-        self.assertRaises(env.CastError, lib.Func.cast, int_value)
+        """Test the FUNCTION type."""
+        self.assertEqual(lib.FUNCTION.cast(USELESS_FUNCTION), USELESS_FUNCTION)
+        self.assertRaises(env.CastError, lib.FUNCTION.cast, INT_VALUE)
 
     def test_list(self):
-        """Test the List type."""
-        self.assertEqual(lib.List.cast(list_value), list_value)
-        self.assertEqual(lib.List.cast(string_value), list_value)
-        self.assertRaises(env.CastError, lib.List.cast, bool_value)
+        """Test the LIST type."""
+        self.assertEqual(lib.LIST.cast(LIST_VALUE), LIST_VALUE)
+        self.assertEqual(lib.LIST.cast(STRING_VALUE), LIST_VALUE)
+        self.assertRaises(env.CastError, lib.LIST.cast, BOOL_VALUE)
 
     def test_set(self):
-        """Test the Set type."""
-        self.assertEqual(lib.Set.cast(set_value), set_value)
-        self.assertEqual(lib.Set.cast(list_value), set_value)
-        self.assertRaises(env.CastError, lib.Set.cast, bool_value)
+        """Test the SET type."""
+        self.assertEqual(lib.SET.cast(SET_VALUE), SET_VALUE)
+        self.assertEqual(lib.SET.cast(LIST_VALUE), SET_VALUE)
+        self.assertRaises(env.CastError, lib.SET.cast, BOOL_VALUE)
 
     def test_object(self):
-        """Test the Object type."""
-        self.assertEqual(lib.Object.cast(object_value), object_value)
-        self.assertRaises(env.CastError, lib.Object.cast, missing_operator)
+        """Test the OBJECT type."""
+        self.assertEqual(lib.OBJECT.cast(OBJECT_VALUE), OBJECT_VALUE)
+        self.assertRaises(env.CastError, lib.OBJECT.cast,
+                          ANOTHER_USELESS_OPERATOR)
 
     def test_add_operation(self):
         """Test the add operator / function."""
-        add_op = lib.AddOperator
+        add_op = lib.ADD_OPERATOR
         context = env.empty_context()
 
         # Case 1: Two int values -> int value
-        args = [
-            int_value,
-            int_value,
-        ]
-        self.assertEqual(add_op.eval(args, context), int2_value)
+        args = [INT_VALUE, INT_VALUE]
+        self.assertEqual(add_op.eval(args, context), INT2_VALUE)
 
         # Case 2: Two float values -> float value
-        args = [
-            float_value,
-            float_value,
-        ]
-        self.assertEqual(add_op.eval(args, context), float2_value)
+        args = [FLOAT_VALUE, FLOAT_VALUE]
+        self.assertEqual(add_op.eval(args, context), FLOAT2_VALUE)
 
         # Case 3: First int, second float -> int
-        args = [
-            int_value,
-            float_value,
-        ]
-        self.assertEqual(add_op.eval(args, context), int2_value)
+        args = [INT_VALUE, FLOAT_VALUE]
+        self.assertEqual(add_op.eval(args, context), INT2_VALUE)
 
         # Case 4: string + int -> string
-        args = [
-            string_value,
-            int_value,
-        ]
-        self.assertEqual(add_op.eval(args, context), string1_value)
+        args = [STRING_VALUE, INT_VALUE]
+        self.assertEqual(add_op.eval(args, context), STRING1_VALUE)
 
     def test_sub_operation(self):
         """Test the sub operator / function."""
-        sub_op = lib.SubOperator
+        sub_op = lib.SUB_OPERATOR
         context = env.empty_context()
 
         # Case 1: Two int values -> int value
-        args = [int2_value, int_value]
-        self.assertEqual(sub_op.eval(args, context), int_value)
+        args = [INT2_VALUE, INT_VALUE]
+        self.assertEqual(sub_op.eval(args, context), INT_VALUE)
         # Case 2: Two float values -> float
-        args = [float2_value, float_value]
-        self.assertEqual(sub_op.eval(args, context), float_value)
+        args = [FLOAT2_VALUE, FLOAT_VALUE]
+        self.assertEqual(sub_op.eval(args, context), FLOAT_VALUE)
         # Case 3: First float, second int -> float
-        args = [float2_value, int_value]
-        self.assertEqual(sub_op.eval(args, context), float_value)
+        args = [FLOAT2_VALUE, INT_VALUE]
+        self.assertEqual(sub_op.eval(args, context), FLOAT_VALUE)
