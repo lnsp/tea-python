661c9413900d74baa308feec3232bc3c9edee361
Lennart Espe <lennart@mooxmirror.io>
2016-04-19 00:35:12 +0200

    Fix bugs and increment version number

--- repl.py
+++ repl.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """Command line runtime for Tea."""
 
-from runtime import tokenizer, parser, ast, std
+import runtime.lib
+from runtime import tokenizer, parser, env
 
-TEA_VERSION = "0.0.3-dev"
+TEA_VERSION = "0.0.4-dev"
 TEA_TITLE = "Tea @" + TEA_VERSION
 CLI_SYMBOL = "#> "
 CLI_SPACE = " " * 3
@@ -15,10 +16,10 @@ def interpret(expression, context):
     if expression == "exit":
         context.flags.append("exit")
         return
-    
+
     tokens = tokenizer.apply(expression)
     tree = parser.generate(tokens)
-    return tree.tree_to_string()
+    return tree.eval(context)
 
 
 def main():
@@ -27,12 +28,14 @@ def main():
     print(TEA_TITLE)
 
     # run REPL
-    context = std.default_context()
+    context = env.empty_context()
+    context.load(runtime.lib)
     while "done" not in context.flags:
         output = interpret(input(CLI_SYMBOL), context)
         while "continue" in context.flags:
             output = interpret(input(CLI_SPACE), context)
-        if "exit" in context.flags: return
+        if "exit" in context.flags:
+            return
         print(output)
 
 if __name__ == "__main__":
