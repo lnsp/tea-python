587071437d6f00b255e8aa00c5b82a6d05dff63e
Lennart Espe <lennart@mooxmirror.io>
2016-11-25 23:53:44 +0100

    Add !exec command for basic code reuse

--- repl.py
+++ repl.py
@@ -2,29 +2,36 @@
 """Command line runtime for Tea."""
 
 import runtime.lib
-from runtime import lexer, parser, env
+from runtime import lexer, parser, env, flags
 
 TEA_VERSION = "0.0.5-dev"
 TEA_TITLE = "Tea @" + TEA_VERSION
-CLI_SYMBOL = "#> "
+CLI_SYMBOL = ">> "
 CLI_SPACE = " " * 3
 CLI_RESULT = "<- "
-
+CLI_ERROR = "!! "
 
 def interpret(expression, context):
     """Interpret an expression by tokenizing, parsing and evaluating."""
-    if expression == "exit":
+    if expression == "!exit":
         context.flags.append("exit")
         return
+    if expression == "!debug":
+        flags.debug = not flags.debug
+        return "Debug mode %s" % ("on" if flags.debug else "off")
+    if expression.startswith("!exec"):
+        # load file
+        filename = expression.split(' ')[1]
+        print("Executing %s" % filename)
+        with open(filename, "r") as f:
+            expression = ' '.join(line for line in f)
 
     try:
         tokens = lexer.run(expression)
-        print('Generated tokens:', ', '.join((str(e) for e in tokens)))
         tree = parser.generate(tokens)
-        print(tree)
-        return tree.eval(context).data
+        return CLI_RESULT + tree.eval(context).format()
     except (env.FunctionException, env.OperatorException, env.RuntimeException, parser.ParseException) as e:
-        return str(e)
+        return CLI_ERROR + str(e)
 
 
 def main():
