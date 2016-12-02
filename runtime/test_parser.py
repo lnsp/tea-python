383226268bea0b9959bffa892d84dbe018149345
Lennart Espe <lennart@mooxmirror.io>
2016-12-02 12:21:59 +0100

    Add tests for get_arg_count

--- runtime/test_parser.py
+++ runtime/test_parser.py
@@ -5,6 +5,9 @@ import unittest
 import runtime.lexer
 from runtime.parser import *
 
+def token(value, kind):
+    return lexer.TokenTuple(value=value, kind=kind)
+
 def clean_lex(expr):
     clean = lexer.run(expr)
     output = []
@@ -42,4 +45,53 @@ class TestParser(unittest.TestCase):
 
         for tc in cases:
             self.assertEqual(find_matching_prt(tc[0], 1), tc[1])
+
+    def test_arg_count(self):
+        plus_token = token("+", lexer.OPERATOR)
+        minus_token = token("-", lexer.OPERATOR)
+        neg_token = token("!", lexer.OPERATOR)
+        and_token = token("&&", lexer.OPERATOR)
+        or_token = token("||", lexer.OPERATOR)
+        xor_token = token("^|", lexer.OPERATOR)
+        equal_token = token("==", lexer.OPERATOR)
+        unequal_token = token("!=", lexer.OPERATOR)
+        smaller_token = token("<", lexer.OPERATOR)
+        larger_token = token(">", lexer.OPERATOR)
+        smequ_token = token("<=", lexer.OPERATOR)
+        lgequ_token = token(">=", lexer.OPERATOR)
+
+        lprt_token = token(")", lexer.LPRT)
+        sep_token = token(",", lexer.SEPARATOR)
+        operation_tokens = [plus_token, minus_token, sep_token, lprt_token, None]
+
+        identifier_token = token("abc", lexer.IDENTIFIER)
+        string_token = token("abc", lexer.STRING)
+        rprt_token = token("(", lexer.RPRT)
+        number_token = token("1", lexer.NUMBER)
+        value_tokens = [identifier_token, string_token, rprt_token, number_token]
+
+        all_tokens = operation_tokens + value_tokens
+
+        cases = [
+            (plus_token, value_tokens, 2),
+            (plus_token, operation_tokens, 1),
+            (minus_token, value_tokens, 2),
+            (minus_token, operation_tokens, 1),
+            (neg_token, value_tokens, 1),
+            (neg_token, operation_tokens, 1),
+            (and_token, all_tokens, 2),
+            (or_token, all_tokens, 2),
+            (xor_token, all_tokens, 2),
+            (equal_token, all_tokens, 2),
+            (unequal_token, all_tokens, 2),
+            (smaller_token, all_tokens, 2),
+            (larger_token, all_tokens, 2),
+            (smequ_token, all_tokens, 2),
+            (lgequ_token, all_tokens, 2),
+        ]
+
+        for tc in cases:
+            for e in tc[1]:
+                self.assertEqual(get_arg_count(tc[0].value, e), tc[2],
+                                 "bad operator arg count for %s when tested against %s" % (tc[0].value, e))
 
